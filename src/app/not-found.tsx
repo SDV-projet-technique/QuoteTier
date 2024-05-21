e9687480fac3681100e079d1d9e60a90d6b9bc63
Pierre Marquet <marquet_pierre@yahoo.fr>
2024-05-21 13:00:48 +0200

    ready for back

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <main className="flex w-full flex-1 items-center justify-center">
-      <div>
-        <h2>Not Found</h2>
-        <p>Could not find requested page</p>
-        <Link href="/">Return Home</Link>
+    <main className="flex w-full flex-1 flex-col items-center justify-center">
+      <Image
+        src="/images/404.png"
+        className="mb-5 rounded-full"
+        alt="404"
+        width={200}
+        height={200}
+      />
+      <div className="text-center text-3xl font-bold">
+        <h2>404 - Page not Found</h2>
+        <Link href="/" className="hover:underline">
+          Click here to return Home
+        </Link>
       </div>
     </main>
   );
